cac0c83668fe13ab95f257d7319537ec8f95124d
yonas <110130+yonas@users.noreply.github.com>
2024-12-23 06:43:12 +0000

    update(cmake): update libs and driver to latest master.
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- cmake/modules/driver.cmake
+++ cmake/modules/driver.cmake
@@ -35,9 +35,9 @@ else()
 	# FALCOSECURITY_LIBS_VERSION. In case you want to test against another driver version (or
 	# branch, or commit) just pass the variable - ie., `cmake -DDRIVER_VERSION=dev ..`
 	if(NOT DRIVER_VERSION)
-		set(DRIVER_VERSION "12c8b09a4c50a8db52b07105d29eb6eee826b3e9")
+		set(DRIVER_VERSION "7b9e76f54eb9929ef2770b20e2c457811d47d9cc")
 		set(DRIVER_CHECKSUM
-			"SHA256=345665169f7fce64af926ef018ad4c62170ff15c6d07ad2f5cb7ff23431f6af1"
+			"SHA256=cb5c56ab03dc58bb215b45a613a1024979607652d813733b13beac5bbcffc4d3"
 		)
 	endif()
 
